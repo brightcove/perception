094c5eb1dbe39adb46eeb4400fe6d4d019387c1c
Jim R. Wilson <jwilson@brightcove.com>
2013-02-08 16:55:08 -0500

    expanding templates, adding routes

--- attachments/scripts/site.js
+++ attachments/scripts/site.js
@@ -8,8 +8,23 @@ var
   // database obj
   db = window.db = $.couch.db(document.location.href.split('/')[3]),
   
+  // list of templates
+  $templates = $('script[type="text/mustache"]'),
+  
+  /**
+   * convenience method for looking up a template and performing a mustache
+   * transformation on it to return HTML.
+   */
+  render = function(selector, data) {
+    var $elem = $templates.filter(selector);
+    if (!$elem.length) {
+      throw "no templates match the selector: " + selector;
+    }
+    return $.mustache($elem.text(), data || {});
+  },
+  
   // initialize the application
-  app = window.app = $.sammy('#main', function() {
+  app = window.app = $.sammy('.main', function() {
     
     // include a plugin
     this.use('Mustache');
@@ -24,15 +39,29 @@ var
     /**
      * main route, show a list of tests
      */
-    this.get('#/', function() {
-      
+    this.get('#/', function(context) {
       db.view('perception/tests', {
         success: function(result){
-          console.log(result);
+          context
+            .$element()
+              .html(render('.list-tests',result));
         }
       });
-      
-      
+    });
+    
+    /**
+     * add a new test
+     */
+    this.get('#/add-test', function(context) {
+      context
+        .$element()
+          .html(render('.add-test'));
+    });
+    
+    /**
+     * handle submission of new test
+     */
+    this.post('#/add-test', function(context) {
     });
     
   });
