c5a55d4819fca2cbc05e6b6e10fb08315cb9e5dc
Jim R. Wilson <jwilson@brightcove.com>
2013-02-08 15:05:24 -0500

    major overhaul to index html and site js

--- attachments/scripts/site.js
+++ attachments/scripts/site.js
@@ -1,60 +1,43 @@
-var request = function (options, callback) {
-  options.success = function (obj) {
-    callback(null, obj);
-  }
-  options.error = function (err) {
-    if (err) callback(err);
-    else callback(true);
-  }
-  if (options.data && typeof options.data == 'object') {
-    options.data = JSON.stringify(options.data)
-  }
-  if (!options.dataType) options.processData = false;
-  if (!options.dataType) options.contentType = 'application/json';
-  if (!options.dataType) options.dataType = 'json';
-  $.ajax(options)
-}
+/**
+ * site
+ */
+(function(window, document, $){
 
-$.expr[":"].exactly = function(obj, index, meta, stack){ 
-  return ($(obj).text() == meta[3])
-}
-
-var param = function( a ) {
-  // Query param builder from jQuery, had to copy out to remove conversion of spaces to +
-  // This is important when converting datastructures to querystrings to send to CouchDB.
-	var s = [];
-	if ( jQuery.isArray(a) || a.jquery ) {
-		jQuery.each( a, function() { add( this.name, this.value ); });		
-	} else { 
-	  for ( var prefix in a ) { buildParams( prefix, a[prefix] ); }
-	}
-  return s.join("&");
-	function buildParams( prefix, obj ) {
-		if ( jQuery.isArray(obj) ) {
-			jQuery.each( obj, function( i, v ) {
-				if (  /\[\]$/.test( prefix ) ) { add( prefix, v );
-				} else { buildParams( prefix + "[" + ( typeof v === "object" || jQuery.isArray(v) ? i : "") +"]", v )}
-			});				
-		} else if (  obj != null && typeof obj === "object" ) {
-			jQuery.each( obj, function( k, v ) { buildParams( prefix + "[" + k + "]", v ); });				
-		} else { add( prefix, obj ); }
-	}
-	function add( key, value ) {
-		value = jQuery.isFunction(value) ? value() : value;
-		s[ s.length ] = encodeURIComponent(key) + "=" + encodeURIComponent(value);
-	}
-}
-
-var app = {};
-app.index = function () {
+var
   
-};
+  // database obj
+  db = window.db = $.couch.db(document.location.href.split('/')[3]),
+  
+  // initialize the application
+  app = window.app = $.sammy('#main', function() {
+    
+    // include a plugin
+    this.use('Mustache');
+    
+    // user actions to implement:
+    // * list existing tests
+    // * add a test
+    // * edit an existing test
+    // * run a test
+    // * view summary statistics and graphs
+    
+    /**
+     * main route, show a list of tests
+     */
+    this.get('#/', function() {
+      
+      db.view('perception/tests', {
+        success: function(result){
+          console.log(result);
+        }
+      });
+      
+      
+    });
+    
+  });
 
-$(function () { 
-  app.s = $.sammy(function () {
-    // Index of all databases
-    this.get('', app.index);
-    this.get("#/", app.index);
-  })
-  app.s.run();
-});
+// start the application
+app.run('#/');
+
+})(window, document, jQuery);
