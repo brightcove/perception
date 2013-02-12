b276b57e441370de2ba5cb2d3b280475454b3527
Jim R. Wilson <jwilson@brightcove.com>
2013-02-11 22:12:47 -0500

    adding histogram for analysis

--- attachments/scripts/site.js
+++ attachments/scripts/site.js
@@ -165,13 +165,93 @@ var
             startkey: doc._id,
             endkey: doc._id,
             success: function(runs) {
-              context.$element().html(render('.analyze-test', {
-                stats: [
-                  { key: 'url', value: doc.url },
-                  { key: 'description', value: doc.description },
-                  { key: 'total runs', value: runs.total_rows }
-                ]
-              }));
+              
+              var
+                
+                // context element (.main)
+                $elem = context.$element(),
+                
+                // extract the delta values
+                values = $.map(runs.rows, function(row){
+                  return row.value.stopTime - row.value.startTime;
+                });
+                
+              // fill in main content, then generate histogram
+              $elem
+                .attr('class', 'main analyze-test')
+                .html(render('.analyze-test', {
+                  stats: [
+                    { key: 'url', value: doc.url },
+                    { key: 'description', value: doc.description },
+                    { key: 'total runs', value: runs.total_rows },
+                    { key: 'median load time', value: d3.round(d3.median(values)) + ' ms' },
+                    { key: 'mean load time', value: d3.round(d3.mean(values)) + ' ms' }
+                  ]
+                }));
+              
+              var
+                
+                // formatter for counts.
+                formatCount = d3.format(",.0f"),
+                
+                // chart dimensions
+                margin = {top: 10, right: 30, bottom: 30, left: 30},
+                width = $elem.width() - margin.left - margin.right,
+                height = 200 - margin.top - margin.bottom,
+                
+                // x scale
+                x = d3.scale.linear()
+                  .domain([0, d3.max(values)])
+                  .range([0, width]),
+                
+                // generate a histogram using twenty uniformly-spaced bins.
+                data = d3.layout.histogram()
+                  .bins(x.ticks(20))
+                  (values),
+                
+                // y scale
+                y = d3.scale.linear()
+                  .domain([0, d3.max(data, function(d) { return d.y; })])
+                  .range([height, 0]);
+                
+                // x-axis
+                xAxis = d3.svg.axis()
+                  .scale(x)
+                  .orient("bottom");
+                
+                // insert svg image for actual chart into context element
+                svg = d3.select($elem[0]).append("svg")
+                  .attr("width", width + margin.left + margin.right)
+                  .attr("height", height + margin.top + margin.bottom)
+                  .append("g")
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")"),
+                
+                // adjust chart bars
+                bar = svg.selectAll(".bar")
+                  .data(data)
+                  .enter().append("g")
+                    .attr("class", "bar")
+                    .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+              
+              // append svg rect to each bar
+              bar.append("rect")
+                  .attr("x", 1)
+                  .attr("width", x(data[0].dx) - 1)
+                  .attr("height", function(d) { return height - y(d.y); });
+              
+              // append formatted text label to each bar
+              bar.append("text")
+                  .attr("dy", ".75em")
+                  .attr("y", 6)
+                  .attr("x", x(data[0].dx) / 2)
+                  .attr("text-anchor", "middle")
+                  .text(function(d) { return formatCount(d.y); });
+              
+              // append x-axis to the image
+              svg.append("g")
+                  .attr("class", "x axis")
+                  .attr("transform", "translate(0," + height + ")")
+                  .call(xAxis);
             }
           });
         }
