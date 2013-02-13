9ae357c4ead46e98dbdd083d06d939b0c936e0d2
David LaPalomento <dlapalomento@gmail.com>
2013-02-12 20:54:43 -0500

    move histogram into a helper function (functorialization)

--- attachments/scripts/site.js
+++ attachments/scripts/site.js
@@ -31,6 +31,79 @@ var
     }
     return Mustache.to_html($elem.text(), data || {});
   },
+
+  /**
+   * render a histogram for test results.
+   */
+  histogram = function(values, $elem) {
+    var
+    
+      // formatter for counts.
+      formatCount = d3.format(",.0f"),
+
+      // calculate high bound for x axis
+      ninetyfifth = d3.quantile(values, 0.95),
+      maxX = ninetyfifth * 1.25,
+      
+      // chart dimensions
+      margin = {top: 10, right: 30, bottom: 30, left: 30},
+      width = $elem.width() - margin.left - margin.right,
+      height = 200 - margin.top - margin.bottom,
+      
+      // x scale
+      x = d3.scale.linear()
+        .domain([0, maxX])
+        .range([0, width]),
+      
+      // generate a histogram using twenty uniformly-spaced bins.
+      data = d3.layout.histogram()
+        .bins(x.ticks(20))
+      (values),
+      
+      // y scale
+      y = d3.scale.linear()
+        .domain([0, d3.max(data, function(d) { return d.y; })])
+        .range([height, 0]);
+      
+      // x-axis
+      xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+      
+      // insert svg image for actual chart into context element
+      svg = d3.select($elem[0]).append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")"),
+      
+      // adjust chart bars
+      bar = svg.selectAll(".bar")
+        .data(data)
+        .enter().append("g")
+        .attr("class", "bar")
+        .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+      
+      // append svg rect to each bar
+      bar.append("rect")
+        .attr("x", 1)
+        .attr("width", x(data[0].dx) - 1)
+        .attr("height", function(d) { return height - y(d.y); });
+      
+      // append formatted text label to each bar
+      bar.append("text")
+        .attr("dy", ".75em")
+        .attr("y", 6)
+        .attr("x", x(data[0].dx) / 2)
+        .attr("text-anchor", "middle")
+        .text(function(d) { return formatCount(d.y); });
+      
+      // append x-axis to the image
+      svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+  },
   
   /**
    * convenience method to attempt to correct legacy test documents.
@@ -178,7 +251,7 @@ var
         }
       });
     },
-    
+
     /**
      * analyze the runs for a given test.
      */
@@ -200,10 +273,6 @@ var
                 values = $.map(runs.rows, function(row){
                   return row.value.stopTime - row.value.startTime;
                 }).sort(d3.ascending),
-                
-                // calculate high bound for x axis
-                ninetyfifth = d3.quantile(values, 0.95),
-                maxX = ninetyfifth * 1.25,
                 
                 // statistics to display
                 stats = [
@@ -230,70 +299,8 @@ var
               if (!values.length) {
                 return;
               }
-              
-              var
-                
-                // formatter for counts.
-                formatCount = d3.format(",.0f"),
-                
-                // chart dimensions
-                margin = {top: 10, right: 30, bottom: 30, left: 30},
-                width = $elem.width() - margin.left - margin.right,
-                height = 200 - margin.top - margin.bottom,
-                
-                // x scale
-                x = d3.scale.linear()
-                  .domain([0, maxX])
-                  .range([0, width]),
-                
-                // generate a histogram using twenty uniformly-spaced bins.
-                data = d3.layout.histogram()
-                  .bins(x.ticks(20))
-                  (values),
-                
-                // y scale
-                y = d3.scale.linear()
-                  .domain([0, d3.max(data, function(d) { return d.y; })])
-                  .range([height, 0]);
-                
-                // x-axis
-                xAxis = d3.svg.axis()
-                  .scale(x)
-                  .orient("bottom");
-                
-                // insert svg image for actual chart into context element
-                svg = d3.select($elem[0]).append("svg")
-                  .attr("width", width + margin.left + margin.right)
-                  .attr("height", height + margin.top + margin.bottom)
-                  .append("g")
-                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")"),
-                
-                // adjust chart bars
-                bar = svg.selectAll(".bar")
-                  .data(data)
-                  .enter().append("g")
-                    .attr("class", "bar")
-                    .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
-              
-              // append svg rect to each bar
-              bar.append("rect")
-                  .attr("x", 1)
-                  .attr("width", x(data[0].dx) - 1)
-                  .attr("height", function(d) { return height - y(d.y); });
-              
-              // append formatted text label to each bar
-              bar.append("text")
-                  .attr("dy", ".75em")
-                  .attr("y", 6)
-                  .attr("x", x(data[0].dx) / 2)
-                  .attr("text-anchor", "middle")
-                  .text(function(d) { return formatCount(d.y); });
-              
-              // append x-axis to the image
-              svg.append("g")
-                  .attr("class", "x axis")
-                  .attr("transform", "translate(0," + height + ")")
-                  .call(xAxis);
+
+              histogram(values, $elem);
             }
           });
         }
